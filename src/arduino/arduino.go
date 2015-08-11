03fcfbcf1eee2d7e8c50fc9a915814330fe96a3b
Xavier Serrano <zombispormedio007@gmail.com>
2015-08-11 22:26:14 +0200

    better

--- src/arduino/arduino.go
+++ src/arduino/arduino.go
@@ -125,7 +125,7 @@ func Action() {
 
 	work := func() {
 		//server_request.Advice()
-		gobot.Every(1*time.Second, func() {
+		gobot.Every(0.5*time.Second, func() {
 			res := server_request.Check()
 switchAll(off)
 			if res.Data != nil {
